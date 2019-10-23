187b1095371cbcb1660a902956a3a583e55456a8
Ravern Koh <ravernkoh@gmail.com>
2019-10-23 17:02:56 +0800

    Get translation working!

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
-function App() {
-  const [language, setLanguage] = useState('es');
-  
-  useEffect(() => {
-    // TODO: Change the language
-  }, [language]);
+import { IntlProvider, defineMessages, FormattedMessage } from "react-intl";
 
-  return <LangProvider client={client}>
+const messages = defineMessages({
+  en: require('../i18n/en/strings.json'),
+  zh: require('../i18n/zh/strings.json'),
+});
+
+function App() {
+  const [language, setLanguage] = useState('en');
+
+  return <IntlProvider locale={language} messages={messages[language]}>
     <select onChange={e => setLanguage(e.target.value)} value={language}>
       <option>en</option>
       <option>zh</option>
     </select>
-    <h1>Hello, world!</h1>
-    <h2>I am Ravern.</h2>
-  </LangProvider>
+    <h1><FormattedMessage id="helloWorld" /></h1>
+    <h2><FormattedMessage id="greeting" values={{ name: "Ravern" }} /></h2>
+  </IntlProvider>
 }
 
 const root = document.getElementById('root');
